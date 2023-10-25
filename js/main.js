3f86751a87111554c83d90302749040dae108625
Leandro Silva <leandro.csilva356@gmail.com>
2023-10-25 03:51:08 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+      $('#telefone').mask('(00) 00000-0000');
+      $('#cpf').mask('000.000.000-00');
+      $('#cep').mask('00000-000');
+
+      $('form').validate({
+            rules: {
+                  nome: {
+                        required: true
+                  },
+                  email: {
+                        required: true,
+                        email: true
+                  },
+                  telefone: {
+                        required: true
+                  },
+                  cpf: {
+                        required: true
+                  },
+                  endereço: {
+                        required: true
+                  },
+                  cep: {
+                        requred: true
+                  }
+            },
+
+            submitHandler: function(form) {
+                  console.log(form)
+            },
+            invalidHandler: function(evento, validador){
+                  let camposIncorretos= validador.numberOfInvalids();
+                  if(camposIncorretos){
+                        alert(`Existem ${camposIncorretos} campos incorretos`)
+                  }
+            }
+      })
+})
